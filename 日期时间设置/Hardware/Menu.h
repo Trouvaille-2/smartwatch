52277cec53ec761deee660e3fd37561641dd4db0
Trouvaille-2 <2015452145@qq.com>
2026-04-16 19:04:38 +0800

    4.16

--- 日期时间设置/Hardware/Menu.h
+++
@@ -1,9 +0,0 @@
-#ifndef __MENU_H
-#define __MENU_H
-
-void Menu_Init(void);
-void Show_clock_ui(void);
-int First_Page_Clock(void);
-int SettingPage(void);
-void SettingPage_ui();
-#endif
--- 首页时钟ui/Hardware/Menu.h
+++
@@ -1,7 +0,0 @@
-#ifndef __MENU_H
-#define __MENU_H
-
-void Menu_Init(void);
-void Show_clock_ui(void);
-int First_Page_Clock(void);
-#endif
